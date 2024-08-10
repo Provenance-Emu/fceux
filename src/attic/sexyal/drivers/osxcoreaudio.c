4e2d04d2f0bd9fb782895181cfce63dc9f27dbf7
Joe Mattiello <mail@joemattiello.com>
2024-08-10 00:36:45 -0400

    Minor include refctor for coreaudio
    Signed-off-by: Joe Mattiello <mail@joemattiello.com>

--- src/attic/sexyal/drivers/osxcoreaudio.c
+++ src/attic/sexyal/drivers/osxcoreaudio.c
@@ -1,5 +1,9 @@
-#include <AudioHardware.h>
-#include <CoreAudioTypes.h>
-#include <HostTime.h>
-
-
+#if !defined(__COREAUDIO_USE_FLAT_INCLUDES__)
+    #include <CoreAudio/CoreAudioTypes.h>
+    #include <CoreAudio/HostTime.h>
+    #include <CoreAudio/AudioHardware.h>
+#else
+    #include <CoreAudioTypes.h>
+    #include <HostTime.h>
+    #include <AudioHardware.h>
+#endif
